a1c22fba0b356dd3595a73ea42f8043291e74a37
Princinho <princegnakou@gmail.com>
2023-04-19 22:31:15 +0000

    style the rentout page

--- src/Pages/RentOut/Rentout.jsx
+++ src/Pages/RentOut/Rentout.jsx
@@ -1,4 +1,4 @@
-import { PinDropSharp } from "@mui/icons-material"
+import { LocationOn, PinDropSharp } from "@mui/icons-material"
 import { useForm } from "react-hook-form"
 import "./Rentout.css"
 export default () => {
@@ -11,18 +11,20 @@ export default () => {
             <h1>Rent out your house</h1>
             <div className="rentout-form-wrapper">
                 <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="flex-hr">
 
-                    <input type="text" {...register("name")} placeholder="Name of the house" title="Name of the house"></input>
-                    <input type="text" {...register("address")} placeholder="Address of the house" title="Address of the house"></input>
+                        <input type="text" {...register("name")} placeholder="Name of the house" title="Name of the house"></input>
+                        <input style={{ marginLeft: "1em" }} type="text" {...register("address")} placeholder="Address of the house" title="Address of the house"></input>
+                    </div>
                     <textarea {...register("about")} placeholder="About the house" title="About the house"></textarea>
 
-
-                    <input type="text" {...register("gps")} placeholder="GPS Coordinates" title="Address of the house"></input>
-                    <button className="action-button property-type active" title="Use current location">
-                        <PinDropSharp className="btn-icon" />
-                    </button>
-
-                    <input type="submit" />
+                    <div className="flex-hr">
+                        <input type="text" {...register("gps")} placeholder="GPS Coordinates" title="Address of the house"></input>
+                        <button className="action-button property-type active" title="Use current location">
+                            <LocationOn className="btn-icon" />
+                        </button>
+                    </div>
+                    <button type="submit" className="action-button" >Save</button>
                 </form>
             </div>
 
