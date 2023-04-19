2398c458e98a27f213d54ec9d668a157772b420e
Princinho <princegnakou@gmail.com>
2023-04-19 21:32:26 +0000

    Add the basic layout for rentout

--- src/Pages/RentOut/Rentout.jsx
+++ src/Pages/RentOut/Rentout.jsx
@@ -1,25 +1,31 @@
 import { PinDropSharp } from "@mui/icons-material"
 import { useForm } from "react-hook-form"
-import { Form } from "react-router-dom"
+import "./Rentout.css"
 export default () => {
     const { register, handleSubmit } = useForm()
+    function onSubmit(data) {
+        console.log(data)
+    }
     return (
         <div>
             <h1>Rent out your house</h1>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div>
+            <div className="rentout-form-wrapper">
+                <form onSubmit={handleSubmit(onSubmit)}>
+
                     <input type="text" {...register("name")} placeholder="Name of the house" title="Name of the house"></input>
                     <input type="text" {...register("address")} placeholder="Address of the house" title="Address of the house"></input>
                     <textarea {...register("about")} placeholder="About the house" title="About the house"></textarea>
-                </div>
-                <div>
+
+
                     <input type="text" {...register("gps")} placeholder="GPS Coordinates" title="Address of the house"></input>
-                    <button className="action-button property-type active">
+                    <button className="action-button property-type active" title="Use current location">
                         <PinDropSharp className="btn-icon" />
-                        <span>House</span>
                     </button>
-                </div>
-            </form>
+
+                    <input type="submit" />
+                </form>
+            </div>
+
         </div>
     )
 }
